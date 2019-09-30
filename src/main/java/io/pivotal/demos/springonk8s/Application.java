deb81d624229ae498e59ef0c01bb68720b5123d3
Alexandre Roman <aroman@pivotal.io>
2019-09-30 22:45:19 +0200

    Update Application.java

--- src/main/java/io/pivotal/demos/springonk8s/Application.java
+++ src/main/java/io/pivotal/demos/springonk8s/Application.java
@@ -36,6 +36,6 @@ class HelloController {
         // Just return a simple String.
         // This app is using reactive programming, but feel free to use legacy
         // REST controllers: it's up to you!
-        return Mono.just("Yo Build Service!");
+        return Mono.just("Hello Pivotal Build Service!");
     }
 }
